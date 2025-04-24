58a3a820a9f07a6b5e5a76a8778b412cac9620ab
pedrovmcastro <pvmcastro@gmail.com>
2025-04-24 02:06:53 -0300

    implement editar e deletar funcionario

--- web/modules/funcionarios.py
+++ web/modules/funcionarios.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from bson.objectid import ObjectId
-from web import secret
-
+import secret
 import time
 import pymongo
 
@@ -9,7 +8,7 @@ client = pymongo.MongoClient(secret.ATLAS_CONNECTION_STRING)
 db = client['achadoseperdidos']
 funcionarios_collections = db['funcionarios']
 funcionario = Blueprint("funcionario", __name__)
-funcionario.secret_key = "-\x06\xb3\xbd\x15/\xc3\xef~\xd8]\xb3\xef\xd8\xa3\xe5\xa5Sn\xf3SNx\xa9"
+
 
 @funcionario.route("/admin/funcionario")
 def funcionario_index():
@@ -23,6 +22,7 @@ def funcionario_index():
 
     return render_template("funcionario.html",
                            funcionarios=funcionarios)
+
 
 @funcionario.route("/admin/funcionario/add", methods=["POST"])
 def funcionario_add():
@@ -51,15 +51,46 @@ def funcionario_add():
 
     return redirect(url_for(".funcionario_index"))
 
+
 @funcionario.route("/admin/funcionario/edit/<string:funcionario_id>")
 def funcionario_edit(funcionario_id):
     return render_template("funcionario_editar.html",
                            funcionario=funcionarios_collections.find_one({"_id": ObjectId(funcionario_id)}))
 
-@funcionario.route("/admin/funcionario/edit", methods=["POST"])
-def funcionario_edit_action():
+
+@funcionario.route("/admin/<string:funcionario_id>/edit", methods=["POST"])
+def funcionario_edit_action(funcionario_id):
+    nome = request.form.get("nome")
+    matricula = request.form.get("matricula")
+    senha = request.form.get("senha")
+    administrador = request.form.get("administrador")
+    rua = request.form.get("rua")
+    cep = request.form.get("cep")
+
+    if nome and matricula and senha and rua and cep:
+        funcionarios_collections.update_one(
+            {"_id": ObjectId(funcionario_id)},
+            {
+                "$set": {
+                    "nome": nome,
+                    "matricula": matricula,
+                    "senha": senha,
+                    "administrador": administrador is not None,
+                    "endereco": {
+                        "rua": rua,
+                        "cep": cep,
+                    },
+                }
+            }
+        )
+        flash("Funcionário atualizado com sucesso!", "good")
+    else:
+        flash("Alguma coisa deu errado!", "bad")
+
     return redirect(url_for(".funcionario_index"))
 
-@funcionario.route("/admin/funcionario/delisgar")
-def funcionario_desligar():
+
+@funcionario.route("/admin/funcionario/desligar/<string:funcionario_id>")
+def funcionario_desligar(funcionario_id):
+    funcionarios_collections.delete_one({"_id": ObjectId(funcionario_id)})
     return redirect(url_for(".funcionario_index"))
