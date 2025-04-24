10309f6a93819aeab39b74eaeb4308ef9b6d68ef
Pedro Cezarini <pedrocezarini13@gmail.com>
2025-04-24 14:05:37 -0300

    adicionando rota para o login

--- web/achados_e_perdidos.py
+++ web/achados_e_perdidos.py
@@ -19,6 +19,10 @@ def index():
 def admin():
     return render_template('admin.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
