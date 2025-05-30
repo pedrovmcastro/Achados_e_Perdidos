ae21546e37c4459b7f70787874fde74fe1e66e9b
pedrovmcastro <142360638+pedrovmcastro@users.noreply.github.com>
2025-05-30 00:45:16 +0000

    Delete web/teste_conexao.py

--- web/teste_conexao.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-uri = "mongodb+srv://mahousenshi:mZo8U6lAgbhpMgFK@cluster0.sn8qnsa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(uri)
-
-try:
-    print(client.list_database_names())
-except Exception as e:
-    print("Erro de conexão:", e)
